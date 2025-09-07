cd1c7081cc4e9361608c09faccd1e598ad60ac3c
Coalery <doralife12@gmail.com>
2025-09-07 16:23:21 +0900

    fix: discovery 기반으로 이벤트 리스너를 등록하도록 수정

--- src/main/kotlin/com/sight/discord/config/DiscordConfig.kt
+++ src/main/kotlin/com/sight/discord/config/DiscordConfig.kt
@@ -1,6 +1,5 @@
 package com.sight.discord.config
 
-import com.sight.controllers.discord.UserDiscordEventController
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.requests.GatewayIntent
@@ -13,11 +12,9 @@ class DiscordConfig {
     @Bean
     fun jda(
         @Value("\${discord.token}") token: String,
-        userDiscordEventController: UserDiscordEventController,
     ): JDA {
         return JDABuilder.createDefault(token)
             .enableIntents(GatewayIntent.GUILD_MEMBERS)
-            .addEventListeners(userDiscordEventController)
             .build()
     }
 }

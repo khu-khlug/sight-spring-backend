3dbb45d31eab1ec76090dd95344f4de1b7048c97
Coalery <doralife12@gmail.com>
2025-09-09 12:45:20 +0900

    fix: cors 허용

--- src/main/kotlin/com/sight/config/WebConfig.kt
+++ src/main/kotlin/com/sight/config/WebConfig.kt
@@ -2,6 +2,7 @@ package com.sight.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
@@ -10,5 +11,9 @@ class WebConfig(
 ) : WebMvcConfigurer {
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
         resolvers.add(requesterArgumentResolver)
+    }
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**").allowedOriginPatterns("*").allowedMethods("*").allowedHeaders("*").allowCredentials(true)
     }
 }

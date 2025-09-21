8cca2817157e41312d227e1c8fb2e1c238ced90a
Coalery <doralife12@gmail.com>
2025-09-21 18:46:17 +0900

    fix: 디스코드 역할 정보로부터 디스코드 역할 아이디를 가져오도록 수정

--- src/main/kotlin/com/sight/service/discord/JdaDiscordApiAdapter.kt
+++ src/main/kotlin/com/sight/service/discord/JdaDiscordApiAdapter.kt
@@ -1,16 +1,22 @@
 package com.sight.service.discord
 
 import com.sight.domain.discord.DiscordRoleType
+import com.sight.repository.DiscordRoleRepository
 import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.entities.Guild
 import net.dv8tion.jda.api.entities.Role
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
+import org.springframework.web.server.ResponseStatusException
 
 @Component
 class JdaDiscordApiAdapter(
     private val jda: JDA,
     @Value("\${discord.guild-id}") private val guildId: String,
+
+    private val discordRoleRepository: DiscordRoleRepository,
 ) : DiscordApiAdapter {
     private val logger = LoggerFactory.getLogger(JdaDiscordApiAdapter::class.java)
 
@@ -53,15 +59,14 @@ class JdaDiscordApiAdapter(
     }
 
     private fun getRoleByDiscordRole(
-        guild: net.dv8tion.jda.api.entities.Guild,
+        guild: Guild,
         discordRole: DiscordRoleType,
     ): Role? {
-        val roleName =
-            when (discordRole) {
-                DiscordRoleType.MEMBER -> "Member"
-                DiscordRoleType.GRADUATED_MEMBER -> "Graduated Member"
-                DiscordRoleType.MANAGER -> "Manager"
-            }
-        return guild.getRolesByName(roleName, true).firstOrNull()
+        val discordRole =
+            discordRoleRepository.findByRoleType(discordRole)
+                ?: throw ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "적절한 디스코드 역할을 찾을 수 없습니다. 운영진에게 문의해주세요.")
+
+        return guild.getRoleById(discordRole.roleId)
+            ?: throw ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "잘못된 디스코드 역할 아이디입니다. 운영진에게 문의해주세요.")
     }
 }

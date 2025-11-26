6585147b213d95b548c5c4ff17b0b46bae83290d
nananina <nananina0415@gmail.com>
2025-11-27 06:43:03 +0900

    feat: 그룹매칭별 응답 목록 조회 API를 위한 메서드 추가

--- src/main/kotlin/com/sight/repository/GroupMatchingAnswerRepository.kt
+++ src/main/kotlin/com/sight/repository/GroupMatchingAnswerRepository.kt
@@ -1,6 +1,16 @@
 package com.sight.repository
 
+import com.sight.domain.group.GroupCategory
 import com.sight.domain.groupmatching.GroupMatchingAnswer
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface GroupMatchingAnswerRepository : JpaRepository<GroupMatchingAnswer, String>
+interface GroupMatchingAnswerRepository : JpaRepository<GroupMatchingAnswer, String> {
+    fun findAllByGroupMatchingId(groupMatchingId: String): List<GroupMatchingAnswer>
+
+    fun findAllByGroupMatchingIdOrderByCreatedAtDesc(groupMatchingId: String): List<GroupMatchingAnswer>
+
+    fun findAllByGroupMatchingIdAndGroupType(
+        groupMatchingId: String,
+        groupType: GroupCategory,
+    ): List<GroupMatchingAnswer>
+}

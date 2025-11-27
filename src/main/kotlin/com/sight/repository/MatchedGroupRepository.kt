3481bfee09b0bbb82b7e5de398d2de62bbb807fc
Coalery <doralife12@gmail.com>
2025-11-27 13:52:57 +0900

    feat: 이미 생성된 그룹에 특정 응답 회원 추가

--- src/main/kotlin/com/sight/repository/MatchedGroupRepository.kt
+++ src/main/kotlin/com/sight/repository/MatchedGroupRepository.kt
@@ -5,4 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 interface MatchedGroupRepository : JpaRepository<MatchedGroup, String> {
     fun findAllByAnswerIdIn(answerIds: List<String>): List<MatchedGroup>
+
+    fun existsByGroupIdAndAnswerId(
+        groupId: Long,
+        answerId: String,
+    ): Boolean
 }

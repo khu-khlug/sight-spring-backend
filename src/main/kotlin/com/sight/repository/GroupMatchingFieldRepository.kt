c8c2cbcd416fee3c978665721a589ffcb1c4cd51
nananina <nananina0415@gmail.com>
2025-11-27 16:27:56 +0900

    feat: GroupMatchingField에 obsoletedAt 필드 추가

--- src/main/kotlin/com/sight/repository/GroupMatchingFieldRepository.kt
+++ src/main/kotlin/com/sight/repository/GroupMatchingFieldRepository.kt
@@ -2,7 +2,10 @@ package com.sight.repository
 
 import com.sight.domain.groupmatching.GroupMatchingField
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Optional
 
 interface GroupMatchingFieldRepository : JpaRepository<GroupMatchingField, String> {
     fun existsByName(name: String): Boolean
+
+    fun findByIdAndObsoletedAtIsNull(id: String): Optional<GroupMatchingField>
 }

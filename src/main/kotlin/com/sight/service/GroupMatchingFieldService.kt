c423f13859141ba07226cbdb334d5ea7ba76c908
parksiwoo2 <siwoo0312@khu.ac.kr>
2025-12-01 01:44:10 +0900

    feat: 관심분야 목록 조회 API 구현

--- src/main/kotlin/com/sight/service/GroupMatchingFieldService.kt
+++ src/main/kotlin/com/sight/service/GroupMatchingFieldService.kt
@@ -2,10 +2,12 @@ package com.sight.service
 
 import com.github.f4b6a3.ulid.UlidCreator
 import com.sight.controllers.http.dto.AddGroupMatchingFieldRequest
+import com.sight.core.auth.UserRole
 import com.sight.core.exception.NotFoundException
 import com.sight.core.exception.UnprocessableEntityException
 import com.sight.domain.groupmatching.GroupMatchingField
 import com.sight.repository.GroupMatchingFieldRepository
+import com.sight.service.dto.GroupMatchingFieldAnswer
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDateTime
@@ -63,5 +65,24 @@ class GroupMatchingFieldService(
     private fun makeFieldActive(field: GroupMatchingField): GroupMatchingField {
         field.obsoletedAt = null
         return groupMatchingFieldRepository.save(field)
+    }
+
+    fun getGroupMatchingFields(userRole: UserRole): List<GroupMatchingFieldAnswer> {
+        val fields: List<GroupMatchingField> =
+            when (userRole) {
+                UserRole.USER -> groupMatchingFieldRepository.findAllByObsoletedAtIsNull()
+                UserRole.MANAGER -> groupMatchingFieldRepository.findAll()
+                else -> emptyList()
+            }
+
+        // 2. Entity -> Service DTO 변환
+        return fields.map { field ->
+            GroupMatchingFieldAnswer(
+                id = field.id,
+                name = field.name,
+                createdAt = field.createdAt,
+                obsoletedAt = field.obsoletedAt,
+            )
+        }
     }
 }

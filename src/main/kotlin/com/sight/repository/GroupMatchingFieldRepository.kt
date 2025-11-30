fc3d0c21e5a8c84efc247ff58a3fb94ab6bfe495
nananina <nananina0415@gmail.com>
2025-11-30 23:38:40 +0900

    chore: 더 이상 사용하지 않는 메서드 삭제

--- src/main/kotlin/com/sight/repository/GroupMatchingFieldRepository.kt
+++ src/main/kotlin/com/sight/repository/GroupMatchingFieldRepository.kt
@@ -2,14 +2,9 @@ package com.sight.repository
 
 import com.sight.domain.groupmatching.GroupMatchingField
 import org.springframework.data.jpa.repository.JpaRepository
-import java.util.Optional
 
 interface GroupMatchingFieldRepository : JpaRepository<GroupMatchingField, String> {
     fun existsByName(name: String): Boolean
-
-    fun existsByNameAndObsoletedAtIsNull(name: String): Boolean
-
-    fun findByIdAndObsoletedAtIsNull(id: String): Optional<GroupMatchingField>
 
     fun findByName(name: String): GroupMatchingField?
 }

20e54143e13be44c3d0cacb00b9b9e5e174ce848
nananina <nananina0415@gmail.com>
2026-04-07 09:02:35 +0900

    fix: ktlint 빈 줄 스타일 수정

--- src/main/kotlin/com/sight/repository/BookBorrowRecordRepository.kt
+++ src/main/kotlin/com/sight/repository/BookBorrowRecordRepository.kt
@@ -13,6 +13,7 @@ interface BookBorrowRecordRepository : JpaRepository<BookBorrowRecord, String> {
     fun findAllByReturnedAtIsNull(): List<BookBorrowRecord>
 
     fun findAllByItemIdInAndReturnedAtIsNull(itemIds: List<String>): List<BookBorrowRecord>
+
     fun findAllByUserIdAndReturnedAtIsNull(userId: Long): List<BookBorrowRecord>
 
     fun findAllByReturnedAtIsNullOrderByBorrowedAtDesc(): List<BookBorrowRecord>

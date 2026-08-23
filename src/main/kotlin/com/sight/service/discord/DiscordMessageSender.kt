3846c8a786af0e5d17ac12735935a421b5c4e16b
Coalery <doralife12@gmail.com>
2026-08-23 14:43:49 +0900

    refactor: Discord DM JDA 구현 통합

--- src/main/kotlin/com/sight/service/discord/DiscordMessageSender.kt
+++ src/main/kotlin/com/sight/service/discord/DiscordMessageSender.kt
@@ -1,35 +1,8 @@
 package com.sight.service.discord
 
-import net.dv8tion.jda.api.JDA
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.stereotype.Component
-import java.util.concurrent.TimeUnit
-
 interface DiscordMessageSender {
     fun sendDirectMessage(
         discordUserId: String,
         content: String,
     )
-}
-
-@Component
-class JdaDiscordMessageSender(
-    @Autowired(required = false) private val jda: JDA?,
-    @param:Value("\${discord.api.timeout:5000}") private val timeoutMillis: Long,
-) : DiscordMessageSender {
-    override fun sendDirectMessage(
-        discordUserId: String,
-        content: String,
-    ) {
-        val user = requireJda().retrieveUserById(discordUserId).submit().get(timeoutMillis, TimeUnit.MILLISECONDS)
-        user.openPrivateChannel()
-            .submit()
-            .get(timeoutMillis, TimeUnit.MILLISECONDS)
-            .sendMessage(content)
-            .submit()
-            .get(timeoutMillis, TimeUnit.MILLISECONDS)
-    }
-
-    private fun requireJda(): JDA = jda ?: throw IllegalStateException("DISCORD_ENABLED=false로 설정되어 Discord 기능을 사용할 수 없습니다.")
 }
